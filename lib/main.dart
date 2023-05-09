c055cf9ca35866f021969db210f37ff7e42c4c14
AbdGhafur <lancengdaol441@gmail.com>
2023-05-09 19:47:01 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tugas8/praktikum1/widgets/myApp.dart';
+import 'package:tugas8/praktikum2/myApp.dart';
+import 'package:tugas8/praktikum3/myApp.dart';
+
+void main() {
+  runApp(const MyApp1());
+}
---
+++ lib/praktikum1/widgets/image.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyImageWidget extends StatelessWidget {
+  const MyImageWidget({
+    Key? key,
+  }) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('images/polinema.png'),
+    );
+  }
+}
